5679d707ed0ec8a2d80543d5949fd33f88a3d102
ErnestAroozoo <45665527+ErnestAroozoo@users.noreply.github.com>
2019-01-06 20:16:00 -0500

    Bomberman images + bomb item

--- Bomberman.java
+++ Bomberman.java
@@ -17,17 +17,17 @@ public class Bomberman implements ActionListener, KeyListener, MouseMotionListen
 	JFrame theframe;
 	BombermanPanel thepanel;
 	Timer thetimer;
-	
+
 	// Methods
 	public void actionPerformed(ActionEvent evt){  // actionPerformed is triggered when JComponents are triggered.
 		if(evt.getSource() == thetimer){ // If timer is going off, 60 FPS in this case, panel will repaint itself.
 			thepanel.repaint();
 		}
 	}
-	
+
 	public void keyReleased(KeyEvent evt){ // keyReleased is triggered when you release the key.
 		if(evt.getKeyCode() == 37){ // Left Arrow Key
-			thepanel.blnLeft = false; 
+			thepanel.blnLeft = false;
 		}
 		else if(evt.getKeyCode() == 38){ // Up Arrow Key
 			thepanel.blnUp = false;
@@ -39,10 +39,10 @@ public class Bomberman implements ActionListener, KeyListener, MouseMotionListen
 			thepanel.blnDown = false;
 		}
 	}
-	
+
 	public void keyPressed(KeyEvent evt){ // keyPressed is triggered when you press the key.
 		if(evt.getKeyCode() == 37){ // Left Arrow Key
-			thepanel.blnLeft = true; 
+			thepanel.blnLeft = true;
 		}
 		else if(evt.getKeyCode() == 38){ // Up Arrow Key
 			thepanel.blnUp = true;
@@ -56,18 +56,18 @@ public class Bomberman implements ActionListener, KeyListener, MouseMotionListen
 			thepanel.blnDown = true;
 		}
 	}
-	
-	public void keyTyped(KeyEvent evt){ // keyTyped is triggered when you press and release the key. 
-		
+
+	public void keyTyped(KeyEvent evt){ // keyTyped is triggered when you press and release the key.
+
 	}
-	
+
 	public void mouseMoved(MouseEvent evt){ // mouseMoved is triggered when mouse is moved.
-		
+
 	}
 	public void mouseDragged(MouseEvent evt){ // mouseDragged is triggered when mouse is dragged.
-	
+
 	}
-	
+
 	// Constructors
 	public Bomberman(){
 		// Create GUI Window
@@ -79,21 +79,20 @@ public class Bomberman implements ActionListener, KeyListener, MouseMotionListen
 		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		theframe.pack();
 		theframe.setVisible(true);
-		
+
 		// Add MouseMotionListener
 		thepanel.addMouseMotionListener(this);
-		
+
 		// Add MouseMotionListener
 		theframe.addKeyListener(this);
-		
+
 		// Add Timer Object
 		thetimer = new Timer(1000/60, this); // Triggering timer object every 1000/60. Basically 60 FPS.
 		thetimer.start();
 	}
-	
+
 	// Main Methods
 	public static void main(String[] args){
 		new Bomberman();
 	}
 }
-
