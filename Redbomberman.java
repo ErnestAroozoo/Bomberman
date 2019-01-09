ef91feed86ca4b673549438375e07883f25e34e1
austinchay <45665569+austinchay@users.noreply.github.com>
2019-01-09 10:04:15 -0500

    Add files via upload

---
+++ Redbomberman.java
@@ -0,0 +1,12 @@
+public class Redbomberman extends bombermanblueprint{
+	// Properties
+	
+	
+	// Methods
+	
+	
+	// Constructor
+	public Redbomberman(
+
+
+}
---
+++ bombermanblueprint.java
@@ -0,0 +1,21 @@
+public class bombermanblueprint{
+	// Properties
+	
+	public String strName;
+	public int intSpeed;
+	public String strColour;
+	public int intbombrange;
+	public int intnumbombs;
+	public boolean blninvincibility;
+	
+	// Constructor
+	public bombermanblueprint(String strName,int intSpeed,String strColour,int intbombrange,int intnumbombs,boolean blninvincibility){
+		this.strName = strName;
+		this.intSpeed = intSpeed;
+		this.strColour = strColour;
+		this.intbombrange = intbombrange;
+		this.intnumbombs = intnumbombs;
+		this.blninvincibility = blninvincibility;
+	
+	}
+}
