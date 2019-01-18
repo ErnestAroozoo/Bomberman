3a4ab58ba672e9da4b2b9dc37d3c3cb3c466a298
austinchay <45665569+austinchay@users.noreply.github.com>
2019-01-18 09:53:54 -0500

    Add files via upload

--- Bombermantools.java
+++ Bombermantools.java
@@ -6,5 +6,22 @@ import javax.imageio.*;
 import java.io.*;
 
 public class Bombermantools{
+
+	
+	public static int validmove(int colfuture, int rowfuture,Graphics g){
+		int validFlag = 0;
 		
+		if((colfuture < 1) || (rowfuture < 0) || (colfuture > 15) || (rowfuture > Bomberman.intLine)){
+			validFlag = 1;
+			
+		}
+		return validFlag;
+	}
+	public static void trysleep(){
+		try{
+			Thread.sleep(1);
+		}catch(InterruptedException e) { 
+		}
+	}
 }
+
